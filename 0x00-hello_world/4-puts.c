4aa4a6c2a6211a0ff697584184dfb41654baa62b
JCZams <jessebiz456@gmail.com>
2022-06-03 04:38:03 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/*
+ *  *main.c file
+ *   *for task 4
+ *   */                                                                                              
+#include <stdio.h>                                                   
+                                                                     
+int main(void) {
+	    
+	   char ch[]= "Programming is like building a multilingual puzzle";
+	   puts(ch);
+   	   return(0);
+}
