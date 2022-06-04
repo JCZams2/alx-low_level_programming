63ffe41f284eb0b55c46f4447933204e9bcbbcfa
JCZams <jessebiz456@gmail.com>
2022-06-03 19:11:40 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+/*
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+
+#include <stdio.h>
+int main(void)
+{
+puts("Size of char: 1 byte(s)");
+puts("size of int: 4 byte(s)");
+puts("size if long long int: 8 byte(s)");
+puts("size if float: 4 byte(s)");
+return (0);
+}
