5ae9b89547390d1dff8ea2f78a8384ce5f956f2a
JCZams <jessebiz456@gmail.com>
2022-06-03 18:40:38 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,9 @@
  *   *
  *    * Return: Always 0 (Success)
  *     */
+#include <stdio.h>
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
