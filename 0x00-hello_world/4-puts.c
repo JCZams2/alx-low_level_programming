4b31a848c86e7b291406e8ef0be7f888b1bf94b2
JCZams <jessebiz456@gmail.com>
2022-06-03 17:39:56 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
-/*using the puts function
+/*
+ * using the puts function
  * task 4
 */
 
@@ -6,5 +7,5 @@
 int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
