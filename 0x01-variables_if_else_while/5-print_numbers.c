451de7724fde20409143b5b4e195cb08d2e2a361
JCZams <jessebiz456@gmail.com>
2022-06-05 04:26:19 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+/**
+ * main - Entry point
+ *
+ * Description: prints numbers
+ *
+ * Return: 0 Always (Success)
+*/
+#include <stdio.h>
+int main(void)
+{
+	for (int i=0; i<10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
