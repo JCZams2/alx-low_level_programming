388b4ffd1949dd4f969df09a7dd0b15ab72b84c2
JCZams <jessebiz456@gmail.com>
2022-06-05 20:25:00 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+
+		if (num < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+
+	putchar('\n');
+	return (0);
+}
