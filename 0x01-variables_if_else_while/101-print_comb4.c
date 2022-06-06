ce0462d312c5b22e2756cba2831f6e0c873e8425
JCZams <jessebiz456@gmail.com>
2022-06-05 21:09:21 -0700

    11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+				if ((i < j) & (j < k) & (k <= '9'))
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if ((i + j + k) < 168)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
