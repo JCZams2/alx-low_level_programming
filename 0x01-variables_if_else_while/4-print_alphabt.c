83183e8fd650fee1b354c2002cf4768c8eb58dbe
JCZams <jessebiz456@gmail.com>
2022-06-05 20:32:54 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,33 @@
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'q')
+		{
+			ch++;
+		}
+		else if (ch == 'e')
+		{
+			ch++;
+		}
+		else
+		{
+			putchar(ch);
+
+			ch++;
+		}
+	}
+
+	putchar(ch);
+	return (0);
+}
