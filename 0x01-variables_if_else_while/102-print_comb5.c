1e6efe90793055cb53b321cb7d470bce5f89223a
JCZams <jessebiz456@gmail.com>
2022-06-06 15:58:31 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -32,7 +32,7 @@ int main(void)
 					putchar(k);
 					putchar(m);
 
-					if (i < 57 || j < 56 || k < 57 || m <57)
+					if (i < 57 || j < 56 || k < 57 || m > 57)
 					{
 						putchar(44);
 						putchar(32);
