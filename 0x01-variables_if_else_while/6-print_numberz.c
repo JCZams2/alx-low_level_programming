e9a66b8996dd44fda13e31f42bf91d1ecde24c8a
JCZams <jessebiz456@gmail.com>
2022-06-05 19:55:12 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+
+		num++;
+	}
+
+	putchar('\n');
+	return (0);
+}
