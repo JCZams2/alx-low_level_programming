5a20c7ec896bd14d08416bdb1f85a02ae59026e1
JCZams <jessebiz456@gmail.com>
2022-06-07 05:05:26 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+/**
+ * main - Entry point
+ *
+ * Description: Printing string with putchar()
+ *
+ * Return: 0 Always (Success)
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	char str[] = "_putchar";
+	
+	int i = 0;
+
+	while (str[i] != '\n')
+	{
+		char s = str[i];
+		putchar(s);
+		i++;
+	}
+	return (0);
+
+}
